4bba668f839c8368d37ccdbd45ab79bad26c3490
Izabela Sojka <sojkaizabela1@gmail.com>
2022-10-27 19:13:23 +0200

    Connect with database and create administratora model

---
+++ ideas/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.conf import settings
+
+class Administrator(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    first_name = models.CharField(("Name"), max_length=50)
+    surname = models.CharField(("Surname"), max_length=50)
+
+    def __str__(self):
+        return 'Administrator profile {}.'.format(self.user.username)
